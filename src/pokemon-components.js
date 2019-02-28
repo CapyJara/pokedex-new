290ffd61d2ffeca68f980ba45272e96b1d12be5b
Jared Myhrberg <jared.myhrberg@gmail.com>
2019-02-28 13:19:11 -0800

    moved make template to new file, tests passing

---
+++ src/pokemon-components.js
@@ -0,0 +1,19 @@
+export function makeHtmlTemplate(pokemon) {
+    const html = `
+    <a href="${pokemon.pokedex}" class="pokemon-container">
+        <div class="${pokemon.type_1} pokemon-div">
+            <div class="${pokemon.type_2} type-two"></div>
+            <h3 id="pokemon">${pokemon.pokemon}</h3>
+            <img src="${pokemon.url_image}">
+            <div class="stats">
+                <p>A: ${pokemon.attack}</p>
+                <p>D: ${pokemon.defense}</p>
+                <p>HP: ${pokemon.hp}</p>
+            </div>
+        </div>
+    </a>`;
+
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
+}
