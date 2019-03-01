463e58de04c8cbda72e92ad8ea77b9b3ac724785
Jared Myhrberg <jared.myhrberg@gmail.com>
2019-03-01 11:38:12 -0800

    no more errors on using filter submit, but not updating pokedex

--- src/index.js
+++ src/index.js
@@ -6,17 +6,44 @@ import pageArray from './paging/page-array.js';
 import sortPokemon from './sorting/sort-pokemon.js';
 import loadSort from './sorting/sort-event.js';
 
+import filterPokemon from './filter/filter-pokemon.js';
+
 
 loadPaging(pokedex.length, pagingOptions => {
     const pagedPokedex = pageArray(pokedex, pagingOptions);
     loadPokemon(pagedPokedex);
 });
 
-
-
 loadSort((sortChoice) => {
     const sortedPokemon = sortPokemon(pokedex, sortChoice);
     loadPaging(sortedPokemon.length, pagingOptions => {
+        const pagedPokedex = pageArray(pokedex, pagingOptions);
+        loadPokemon(pagedPokedex);
+    });
+});
+
+
+const filterForm = document.getElementById('filter-form');
+
+function loadFilter(callback) {
+
+    filterForm.addEventListener('submit', event => {
+        event.preventDefault();
+        
+        const formDaddy = new FormData(filterForm);
+        
+        const filter = {
+            type: formDaddy.get('type-filter'),
+            generation: formDaddy.get('generation-filter')
+        };
+        
+        callback(filter);
+    });
+}
+
+loadFilter(filter => {
+    const filteredList = filterPokemon(filter, pokedex);
+    loadPaging(filteredList.length, pagingOptions => {
         const pagedPokedex = pageArray(pokedex, pagingOptions);
         loadPokemon(pagedPokedex);
     });
