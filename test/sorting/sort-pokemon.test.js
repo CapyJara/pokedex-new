d48c834f98d1ab380272c6718a3a4ae1dc373f51
Jared Myhrberg <jared.myhrberg@gmail.com>
2019-03-01 09:32:18 -0800

    work in progress

--- test/sorting/sort-pokemon.test.js
+++ test/sorting/sort-pokemon.test.js
@@ -17,9 +17,9 @@ test('sorting by id', assert => {
     const results = sortPokemon(poxedex, sortChoice);
     // assert
     assert.deepEqual(results, [
-        { id: 2, height: 70, hp: 100 },
+        { id: 15, height: 50, hp: 80 },
         { id: 8, height: 100, hp: 40 },
-        { id: 15, height: 50, hp: 80 }
+        { id: 2, height: 70, hp: 100 }
     ]);
 });
 
@@ -30,9 +30,9 @@ test('sorting by height', assert => {
     const results = sortPokemon(poxedex, sortChoice);
     // assert
     assert.deepEqual(results, [
-        { id: 15, height: 50, hp: 80 },
+        { id: 8, height: 100, hp: 40 },
         { id: 2, height: 70, hp: 100 },
-        { id: 8, height: 100, hp: 40 }
+        { id: 15, height: 50, hp: 80 }
     ]);
 });
 
@@ -43,8 +43,8 @@ test('sorting by hp', assert => {
     const results = sortPokemon(poxedex, sortChoice);
     // assert
     assert.deepEqual(results, [
-        { id: 8, height: 100, hp: 40 },
+        { id: 2, height: 70, hp: 100 },
         { id: 15, height: 50, hp: 80 },
-        { id: 2, height: 70, hp: 100 }
+        { id: 8, height: 100, hp: 40 }
     ]);
 });
--- test/tests.js
+++ test/tests.js
@@ -3,3 +3,5 @@ import './paging/pokemon-components.test.js';
 import './paging/page-array.test.js';
 
 import './sorting/sort-pokemon.test.js';
+
+// import './filter/filter.test.js';
