0df57232e1a76a397c1caa548d3a34ffca19d98b
Jared Myhrberg <jared.myhrberg@gmail.com>
2019-02-28 17:18:10 -0800

    input button value cleared and placeholder reset properly

--- src/paging-component.js
+++ src/paging-component.js
@@ -1,31 +1,45 @@
 const previousButton = document.getElementById('previous-button');
 const nextButton = document.getElementById('next-button');
-const currentPage = document.getElementById('current-page');
+// const currentPage = document.getElementById('current-page');
 const totalPages = document.getElementById('total-pages');
+const pageTo = document.getElementById('page-to');
 
-const PER_PAGE = 25;
+const PER_PAGE = 18;
 
 export default function loadPaging(totalCount, callback) {
     const totalPageCount = Math.ceil(totalCount / PER_PAGE);
 
     totalPages.textContent = totalPageCount;
     let currentPageNumber = 1;
+    pageTo.placeholder = 1;
     
     updatePaging();
     
     nextButton.addEventListener('click', () => {
         currentPageNumber++;
+        pageTo.placeholder++;
+        pageTo.value = '';
+        pageTo.placeholder = currentPageNumber;
         updatePaging();
     });
     
     previousButton.addEventListener('click', () => {
         currentPageNumber--;
+        pageTo.placeholder--;
+        pageTo.value = '';
+        pageTo.placeholder = currentPageNumber;
+        updatePaging();
+    });
+    
+    pageTo.addEventListener('change', event => {
+        event.preventDefault();
+        currentPageNumber = pageTo.value;
         updatePaging();
     });
     
     
     function updatePaging() {
-        currentPage.textContent = currentPageNumber;
+        pageTo.placeholder = currentPageNumber;
         nextButton.disabled = currentPageNumber === totalPageCount;
         previousButton.disabled = currentPageNumber === 1;
 
